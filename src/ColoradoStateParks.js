e13c09f29488a605a0e583fcee2f431dafcd43ce
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-08 13:42:45 -0400

    Import/Export Code-Along

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+// import { trees, wildlife } from "./parks/RockyMountain";
+// import * as RMFunctions from "./parks/RockyMountain";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(parkTrees)
+  parkWildlife()
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks;
